841c763af8abebbae056f5be2e5e661745e8cdf7
Daniel Bradtmöller <bradtmoeller@googlemail.com>
2023-10-22 22:06:01 +0200

    Create Hello_oop.py
    "Hello, World!" example using object-oriented programming (OOP) in Python

---
+++ Hello_oop.py
@@ -0,0 +1,12 @@
+class HelloWorld:
+    def __init__(self):
+        pass  # Constructor
+
+    def say_hello(self):
+        print("Hello, World!")
+
+# Create an instance of the HelloWorld class
+hello = HelloWorld()
+
+# Call the greet method to print the message
+hello.say_hello()
